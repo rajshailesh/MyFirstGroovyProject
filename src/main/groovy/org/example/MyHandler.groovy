5cd29ebc55ef773a1e7f277fc1389f901bf6902f
Shailesh Raj <shaileshraj@hotmail.com>
2025-05-08 16:14:01 +0530

    modified

--- src/main/groovy/org/example/MyHandler.groovy
+++ src/main/groovy/org/example/MyHandler.groovy
@@ -2,145 +2,83 @@ package org.example
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.events.S3Event
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
+
 import com.amazonaws.services.s3.model.ObjectMetadata
 import com.amazonaws.services.s3.model.PutObjectRequest
-import org.json.JSONObject
 import com.amazonaws.util.IOUtils
 import com.fasterxml.jackson.databind.ObjectMapper
 import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
+import org.json.JSONArray
+import org.json.JSONObject
 
 import java.nio.charset.StandardCharsets
 
-public class MyHandler{
+// Lambda function handler
+public String handleEvent(S3Event event, Context context) {
+  def TARGET_BUCKET = System.getenv('SUMMARY_BUCKET_NAME')
+  def OUTPUT_OBJECT_KEY = "REPORT_SUMMARY"
+  // Get S3 client
+  def s3Client = AmazonS3ClientBuilder.standard().build()
+  // Extract details from S3 event
+  event.getRecords().each { S3EventNotification.S3EventNotificationRecord record ->
+    def sourceBucketName = record.getS3().getBucket().getName()
+    def sourceObjectKey = record.getS3().getObject().getKey()
 
-    static void handleFileProcessing(File file, Map mainMap) {
-    def jsonSlurper = new JsonSlurper()
-    def json = jsonSlurper.parse(file)
-    def percentage = (json.numPassedTests / json.numTotalTests) * 100 + "%"
-    def summary = [resultFileNameName: file.name, percentageofPassedTests: [percentage: percentage]]
-    mainMap.put(file.name, percentage)
-  }
+    // 1. Read data from source S3 object
+    def s3Object = s3Client.getObject(sourceBucketName, sourceObjectKey)
+    def s3InputStream = s3Object.getObjectContent()
+    // 2. Process the data (e.g., add some details, create new JSON)
+    def objectMapper = new ObjectMapper()
+    def newJsonObject = objectMapper.readValue(s3InputStream, Object.class)
 
-  public static String handleEvent(S3Event event, Context context) {
-    def jsonString = "";
-
-
-        if(event != null && event.getRecords().size() > 0){
-
-          def s3EventRecord = event.getRecords().get(0)
-          def s3Bucket = s3EventRecord.getS3().getBucket().getName()
-          def s3ObjectKey = s3EventRecord.getS3().getObject().getKey()
-
-          def s3 = AmazonS3ClientBuilder.standard().withRegion("us-east-2").build()
-          def s3Object = s3.getObject(s3Bucket, s3ObjectKey)
-          def s3InputStream = s3Object.getObjectContent()
-
-          def objectMapper = new ObjectMapper()
-          def jsonObject = objectMapper.readValue(s3InputStream, Object.class)
-
-          def summary
-          def mainMap = [:]
-          def percentage
-
-          percentage = (jsonObject.numPassedTests / jsonObject.numTotalTests) * 100 + "%"
-          summary = [resultFileNameName: s3ObjectKey, percentageofPassedTests:[percentage: percentage]]
-          mainMap.put(s3ObjectKey, percentage)
-
-          jsonString = JsonOutput.toJson(mainMap)
-          jsonString = JsonOutput.prettyPrint(jsonString)
-
-          println("---------------------")
-          println("JSON containing % successful test for corresponding build...\n")
-          println("---------------------")
-          println(jsonString)
-          def summaryBucketName = System.getenv('SUMMARY_BUCKET_NAME')
-          println("Bucket Name: " + summaryBucketName)
-          def objectKey = "REPORT_SUMMARY"
-          // Create the object metadata
-          def metadata = new ObjectMetadata()
-          metadata.setContentType("application/json") // Important for JSON files
-          // Create a PutObjectRequest
-          def putObjectRequest = new PutObjectRequest(summaryBucketName, objectKey, new ByteArrayInputStream(jsonString.getBytes()), metadata)
-          def newObjectContent = new ByteArrayInputStream(jsonString.getBytes(StandardCharsets.UTF_8))
-
-// Upload the JSON file to S3
-          try {
-            def objectExists = s3.doesObjectExist(summaryBucketName, objectKey)
-            if (objectExists) {
-              // Read the existing content
-              def existingObject = s3.getObject(summaryBucketName, objectKey)
-              def existingContent = existingObject.getObjectContent()
-              //def existingContentStr = IOUtils.toString(existingContent, StandardCharsets.UTF_8)
-              def existingContentStr = IOUtils.toString(existingContent)
-              def existingJson = new JSONObject(existingContentStr)
-
-              // Append the new content to the existing content
-              existingJson.append("data", jsonString)  // Or add other logic to combine the data
-              jsonString = existingJson.toString()
-              newObjectContent = new ByteArrayInputStream(jsonString.getBytes(StandardCharsets.UTF_8))
-            }
-            s3.putObject(new PutObjectRequest(summaryBucketName, objectKey, newObjectContent, metadata));
-            context.getLogger().log("Successfully processed and uploaded to: " + summaryBucketName + "/" + objectKey)
-            return "Successfully processed S3 event"
-          } catch (Exception e) {
-            context.getLogger().log("Error processing S3 event: " + e.getMessage())
-            return "Error: " + e.getMessage()
-          } finally {
-            s3.shutdown(); // Close the client
-          }
-
-          println("S3 Event: " + s3EventRecord)
-          println("JSON Data: " + jsonObject)
-          println("Function executed successfully.")
-        }else {
-          println("event object has no records")
-        }
-
-
-    return "return from method:: " + jsonString
-  }
-
-  static void main(String[] args) {
-
-    def projectRoot = new File(".") // current directory
-    def resourcesDir = new File(projectRoot.getAbsoluteFile(), "../resources")
-    def jsonSlurper = new JsonSlurper()
-    def object = new Object()
+    def summaryJSON = new JSONObject()
     def percentage
-    def json
-    def summary
-    def mainMap = [:]
-    if (resourcesDir.exists() && resourcesDir.isDirectory()) {
-      File[] files = resourcesDir.listFiles()
-      println("List of input files...\n")
-      if (files != null) {
-        files.each { file ->
-          println file.name
-          json = new JsonSlurper().parse(file)
-          percentage = (json.numPassedTests / json.numTotalTests) * 100 + "%"
-          summary = [resultFileNameName: file.name, percentageofPassedTests:[percentage: percentage]]
-          mainMap.put(file.name, percentage)
+    JSONArray arr = new JSONArray()
 
-        }
-      } else {
-        println "The resources directory is empty."
+    percentage = (newJsonObject.numPassedTests / newJsonObject.numTotalTests) * 100 + "%"
+    arr.put(percentage)
+    summaryJSON.put(sourceObjectKey, arr)
+
+    def jsonString = summaryJSON.toString()
+
+    def newObjectContent = new ByteArrayInputStream(jsonString.getBytes(StandardCharsets.UTF_8))
+
+    // 3. Create a new ObjectMetadata
+    def objectMetadata = new ObjectMetadata()
+    objectMetadata.setContentType("application/json")
+
+    // 4. Write to a new S3 object (append if the target key exists)
+    try {
+      // Check if the object exists in the target bucket
+      def objectExists = s3Client.doesObjectExist(TARGET_BUCKET, OUTPUT_OBJECT_KEY)
+
+      // Append to the object if it exists, else create a new object
+      if (objectExists) {
+        // Read the existing content
+        def existingObject = s3Client.getObject(TARGET_BUCKET, OUTPUT_OBJECT_KEY)
+        def existingContent = existingObject.getObjectContent()
+        def existingContentStr = IOUtils.toString(existingContent)
+        def existingJson = new JSONObject(existingContentStr)
+
+        // Append the new content to the existing content
+        existingJson.append(sourceObjectKey, percentage)  // Or add other logic to combine the data
+        println(existingJson)
+        jsonString = existingJson.toString()
+        newObjectContent = new ByteArrayInputStream(jsonString.getBytes(StandardCharsets.UTF_8))
       }
-    } else {
-      println "Resources directory not found or is not a directory."
-    }
-    def jsonString = JsonOutput.toJson(mainMap)
-    jsonString = JsonOutput.prettyPrint(jsonString)
-    println("---------------------")
-    println("JSON containing % successful test for corresponding build...\n")
-    println("---------------------")
-    println(jsonString)
-    //def json1 = new JsonSlurper().parse(new File(getClass().getResource("test-results.json").toURI()))
 
-    //println(json1.get('numTotalTests'))
-    //println(json1)
+      // Create the new object with either the new or appended content
+      s3Client.putObject(new PutObjectRequest(TARGET_BUCKET, OUTPUT_OBJECT_KEY, newObjectContent, objectMetadata))
+      context.getLogger().log("Successfully processed and uploaded to: " + TARGET_BUCKET + "/" + OUTPUT_OBJECT_KEY)
+      return "Successfully processed S3 event"
+    } catch (Exception e) {
+      context.getLogger().log("Error processing S3 event: " + e.getMessage())
+      return "Error: " + e.getMessage()
+    }
+
   }
 
+  return "Completed processing S3 event"
 }
-
